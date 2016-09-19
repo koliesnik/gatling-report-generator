3b06cd1c1a2690127a110265bb7bb4c5a43f05a4
pufik <pufik@mailpd.com>
2016-09-19 17:21:44 +0300

    date fix

--- graph_builder.py
+++ graph_builder.py
@@ -7,39 +7,44 @@ class GraphBuilder:
     def __init__(self, raw_data, dirs_path):
         self.raw = raw_data
         self.dirs_path = dirs_path
-        self.mean_trace = self.__get_mean_trace()
 
     def export(self, filename):
-        layout = self.__get_layout(self.mean_trace)
-        data = [self.mean_trace]
+        layout = self.__get_layout()
+        data = [self.__get_mean_trace(False)]
 
         figure = graph.Figure(data=data, layout=layout)
         plotly.offline.plot(figure, filename=filename)
 
-    def __get_mean_trace(self):
+    def __get_mean_trace(self, timestamps=True):
         x = []
         y = []
 
         items = sorted(self.raw.items())
         for timestamp, stats in items:
-            x.append(self.__date(timestamp))
+            if (timestamps):
+                x.append(timestamp)
+            else:
+                x.append(self.__date(timestamp))
+
             y.append(stats['meanResponseTime']['total'])
 
         return dict(x=x, y=y)
 
-    def __get_layout(self, trace):
+    def __get_layout(self):
+        trace = self.__get_mean_trace()
+        print(trace)
         coords = zip(trace['x'], trace['y'])
         annotations = []
 
         for item in coords:
-            report_html = self.dirs_path[:-1] + str(item[1]) + '/index.html'
+            report_html = self.dirs_path[:-1] + item[0] + '/index.html'
             annotations.append(
                 dict(
-                    x=item[0],
+                    x=self.__date(item[0]),
                     y=item[1],
                     xref='x',
                     yref='y',
-                    text='<a href=\"' + report_html + '\">' + item[0] + '</a>',
+                    text='<a href=\"' + report_html + '\">' + self.__date(item[0]) + '</a>',
                     showarrow=True,
                     arrowhead=7,
                 )
